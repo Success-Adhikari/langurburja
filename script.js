d65a016f4fb888e57ae43487ba0e26a301462b58
Success-Adhikari <97819709+Success-Adhikari@users.noreply.github.com>
2022-10-20 18:11:40 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+let total = ["Hukum","Bhurja","Chidi","Etta","Jhandi","Pan"]
+var fund = 100;
+var mon,sts;    
+var fundTxt = document.getElementById('funds');
+
+funding();
+function funding(){
+fundTxt.innerHTML = fund;
+if(fund <= 0){
+    alert("Game Over");
+    location.reload();
+}
+}
+function Roll(){
+var slted=0;
+    var select = document.getElementById('choose');
+    var select2 = document.getElementById('money');
+    var value = select.options[select.selectedIndex].value;
+    var money = select2.options[select2.selectedIndex].value;
+
+    for(var i =1;i<=6;i++){
+    var a = Math.floor(Math.random()*total.length);
+    document.getElementById("d"+i).src = total[a] +".png";
+    if(value == total[a]){
+        slted = slted + 1;
+    }
+    }
+    console.log(slted)
+
+    if( slted > 1){
+     mon = slted*money;
+    }else{
+        mon = 0
+    }
+    if(mon>money){
+        sts = "+$"+(mon-money);
+    }else{
+    sts = "-$"+(money-mon);
+    }
+    fund = fund + (mon-money);
+funding();
+    document.getElementById('gain').innerHTML = sts;
+}
