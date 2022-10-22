9ef313b4284a8f53347ca3e33ec18216dd9b76fd
Success-Adhikari <97819709+Success-Adhikari@users.noreply.github.com>
2022-10-22 20:59:47 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let total = ["Hukum","Bhurja","Chidi","Etta","Jhandi","Pan"]
 var fund = 100;
 var mon,sts;    
+var audio = new Audio('sound.mp3');
 var fundTxt = document.getElementById('funds');
 
 funding();
@@ -12,22 +13,36 @@ if(fund <= 0){
 }
 }
 function Roll(){
+    audio.play();
+
+    for(var i =1;i<=6;i++){
+    document.getElementById('d'+i).style.animationPlayState = "running"
+    }
+    setTimeout( main,4000);
+    function main(){
 var slted=0;
     var select = document.getElementById('choose');
     var select2 = document.getElementById('money');
     var value = select.options[select.selectedIndex].value;
     var money = select2.options[select2.selectedIndex].value;
+    if(fund < money){
+        alert("Insufficient Money")
+        return;
+    }
+
 
     for(var i =1;i<=6;i++){
     var a = Math.floor(Math.random()*total.length);
+  
     document.getElementById("d"+i).src = total[a].toLocaleLowerCase() +".png";
     if(value == total[a]){
         slted = slted + 1;
     }
-    }
-    console.log(slted)
+   }
+  
 
-    if( slted > 1){
+
+   if( slted > 1){
      mon = slted*money;
     }else{
         mon = 0
@@ -40,4 +55,10 @@ var slted=0;
     fund = fund + (mon-money);
 funding();
     document.getElementById('gain').innerHTML = sts;
+
+    for(var i =1;i<=6;i++){
+        document.getElementById('d'+i).style.animationPlayState = "paused"
+        }
+}
+    
 }
